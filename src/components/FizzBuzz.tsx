10562f996d674f307100b44ef796fdbdd7ca5eb5
Melissa Rose <123melissa321@gmail.com>
2022-07-20 09:40:11 +0100

    Updates useState names to conventional naming

--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -2,19 +2,16 @@ import { useState } from "react";
 import { fizzbuzzify } from "../utils/fizzbuzzify";
 
 function FizzBuzz(): JSX.Element {
-  const [previousNumbers, rerenderWithNewNumber] = useState<number[]>([]);
+  const [numberList, setNumberList] = useState<number[]>([]);
 
   const handleNewNumber = () => {
-    return rerenderWithNewNumber([
-      ...previousNumbers,
-      previousNumbers.length + 1,
-    ]);
+    return setNumberList([...numberList, numberList.length + 1]);
   };
 
   return (
     <>
       <h1>FizzBuzz!</h1>
-      <p>{previousNumbers.map(fizzbuzzify)}</p>
+      <p>{numberList.map(fizzbuzzify)}</p>
       <button onClick={handleNewNumber}>Next</button>
     </>
   );
